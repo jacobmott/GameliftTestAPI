4bf8893ede4b9b5056beda7b49da7bfdb11543a0
jacobmott <jacobmott@gmail.com>
2023-08-20 01:56:46 -0500

    Pushing all code for Lambdas for gamelift backend

---
+++ ExchangeCodeForTokens.js
@@ -0,0 +1,79 @@
+const https = require('https');
+const querystring = require('querystring');
+
+const hostname = '';
+const clientId = '';
+const callbackUrl = '';
+
+exports.handler = async (event) => {
+    let response;
+    let code;
+    
+    if (event.body) {
+        const body = JSON.parse(event.body);
+        if (body.code) {
+            code = body.code;
+        }
+    }
+    
+    if (!code) {
+        response = {
+            statusCode: 400,
+            body: JSON.stringify({
+                error: 'incoming request did not have a code'
+            })
+        };
+        return response;
+    }
+    
+    const options = {
+        'hostname': hostname,
+        port: 443,
+        path: '/oauth2/token',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    };
+    
+    const tokenRequestData = querystring.stringify({
+        'code': code,
+        grant_type: 'authorization_code',
+        client_id: clientId,
+        redirect_uri: callbackUrl
+    });
+    
+    const cognitoTokenRequest = new Promise((resolve, reject) => {
+        const req = https.request(options, (res) => {
+            res.setEncoding('utf8');
+            
+            res.on('data', (data) => {
+                return resolve(data);
+            });
+        });
+        
+        req.on('error', (error) => {
+            console.log(error);
+            return reject(error);
+        });
+        
+        req.write(tokenRequestData);
+        req.end();
+    });
+    
+    try {
+        const cognitoTokenResponse = await cognitoTokenRequest;
+        response = {
+            statusCode: 200,
+            body: cognitoTokenResponse
+        };
+    } catch (error) {
+        response = {
+            statusCode: 400,
+            body: JSON.stringify({
+                error: 'request to oauth token endpoint failed'  
+            })
+        };
+    }
+    return response;
+};
